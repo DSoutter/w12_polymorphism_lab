f5570ffe0d604eebe0d6ae6b6b7e99054be0142d
DSoutter <duncan.soutter@gmail.com>
2021-10-04 14:04:49 +0100

    name test passing

--- src/main/java/OnlineAccount.java
+++ src/main/java/OnlineAccount.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class OnlineAccount {
+
+    private String name;
+    private ArrayList<PaymentCard> paymentMethods;
+    private ArrayList<Double> transactionFees;
+
+
+    public OnlineAccount(String name) {
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/test/java/OnlineAccountTest.java
+++ src/test/java/OnlineAccountTest.java
@@ -1,2 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class OnlineAccountTest {
+    OnlineAccount onlineAccount;
+
+    @Before
+    public void before(){
+        onlineAccount = new OnlineAccount("Jim");
+    }
+
+    @Test
+    public void accountHasName(){
+        assertEquals("Jim", onlineAccount.getName());
+    }
 }
